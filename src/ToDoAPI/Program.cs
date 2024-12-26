39c8f8124d53a7a044c97c073371702a00592024
Sudhakar Dahatkar <sudhakar.dahatkar@gmail.com>
2024-12-26 15:38:46 +0000

    added endpoints

--- src/ToDoAPI/Program.cs
+++ src/ToDoAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Data;
+using ToDoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,5 +18,18 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("api/todo", async (AppDbContext context) =>
+{
+    var items = await context.ToDoItems.ToListAsync();
+    return Results.Ok(items);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDoItem todoItem) =>
+{
+    await context.ToDoItems.AddAsync(todoItem);
+    await context.SaveChangesAsync();
+    return Results.Created($"api/todo/{todoItem.Id}", todoItem);
+});
 
 app.Run();
